b8279551febcd5e31aaf761a8f0acf1d157582ff
Ravin Samudika <raveensamudika@gmail.com>
2024-11-21 22:17:58 +0530

    add files

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -10,9 +10,11 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWorks _unitOfWorks;
-        public ProductController(IUnitOfWorks unitOfWorks)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWorks unitOfWorks , IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWorks = unitOfWorks;
+            _webHostEnvironment = webHostEnvironment;
 
         }
         public IActionResult Index()
@@ -22,7 +24,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -33,16 +35,41 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 }),
                 Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product= _unitOfWorks.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+
+           
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
 
 
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath,@"images\Product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, filename),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\Product\" + filename;
+                }
+
+
 
                 _unitOfWorks.Product.Add(productVM.Product);
                 _unitOfWorks.Save();
@@ -62,36 +89,9 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
               
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
+     
 
-            Product? productFormDb = _unitOfWorks.Product.Get(u => u.Id == id);
-            if (productFormDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFormDb);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-
-                _unitOfWorks.Product.Update(obj);
-                _unitOfWorks.Save();
-                TempData["success"] = "Product Updated Succesfully";
-                return RedirectToAction("Index");
-            }
-
-            return View();
-
-        }
+    
 
 
         public IActionResult Delete(int? id)
